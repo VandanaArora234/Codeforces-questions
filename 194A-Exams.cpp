b615f15f0085a2cccec7c8e11cf206451e47dc94
Vandana <aroravandana234@gmail.com>
2021-08-16 23:27:42 +0530

    codeforces solutions

---
+++ 194A-Exams.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+#include <cmath>
+#include <algorithm>
+#define ll long long
+#define asort(a) sort(a,a+n)
+#define dsort(a) sort(a,a+n,greater<int>())
+#define f1 ios_base::sync_with_stdio(false);
+#define f2 cin.tie(NULL);
+using namespace std;
+ 
+ 
+ 
+// Codeforces Round #122 (Div. 2), problem: A. Exams
+ 
+ 
+ 
+int main()
+ 
+{
+     ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+ 
+ 
+int a,b;
+cin>>a>>b; 
+
+cout<<max(3*a-b,0);
+
+
+}
