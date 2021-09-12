6101f363896d65df29e7efa7ae2fea915e0ab906
Vandana <aroravandana234@gmail.com>
2021-09-12 23:44:03 +0530

    codeforces question

---
+++ 1369A-Fashionablee.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+#include <cmath>
+#include <algorithm>
+#define ll long long
+#define asort(a) sort(a,a+n)
+#define dsort(a) sort(a,a+n,greater<int>())
+#define f1 ios_base::sync_with_stdio(false);
+#define f2 cin.tie(NULL);
+using namespace std;
+ 
+
+ // Codeforces Round #652 (Div. 2): A. FashionabLee
+
+int main()
+ 
+{
+     ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+    
+   ll int t;
+   cin>>t;
+   
+   while(t--)
+   {
+       ll int n;cin>>n;
+       
+       if(n%4==0)
+       cout<<"YES"<<endl;
+       
+       else
+       cout<<"NO"<<endl;
+   }
+    
+}
