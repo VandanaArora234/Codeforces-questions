501206b37c2303c442f78989382bf062c9ee78bf
Vandana Arora <62245427+vandana-07@users.noreply.github.com>
2021-09-18 15:25:31 +0530

    Add files via upload

---
+++ 200B-Drinks.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> 
+#include <cmath>
+#include <algorithm>
+#define ll long long
+#define asort(a) sort(a,a+n)
+#define dsort(a) sort(a,a+n,greater<int>())
+#define f1 ios_base::sync_with_stdio(false);
+#define f2 cin.tie(NULL);
+using namespace std;
+ 
+ 
+ //  Codeforces Round #223 (Div. 2): B. Drinks
+ 
+int main()
+ 
+{
+     ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+    
+int n;
+cin>>n;
+
+double sm = 0;
+double arr[n];
+for(int i=0;i<n;i++){
+	cin>>arr[i];
+	sm += (arr[i]/100);
+}
+
+cout<<((sm/n)*100)<<endl;
+ 
+}
